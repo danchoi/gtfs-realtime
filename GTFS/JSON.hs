d327982baa785dfc0e58c1a9fc8a5a72cbd33cd6
Daniel Choi <dhchoi@gmail.com>
2015-01-07 18:44:53 -0500

    spec Alert and VehiclePosition, not tested yet

--- GTFS/JSON.hs
+++ GTFS/JSON.hs
@@ -7,32 +7,32 @@ import Data.ProtocolBuffers
 
 instance ToJSON TripDescriptor where
   toJSON v = object [
-      "trip_id" .= (getField . trip_id $ v)
-    , "route_id" .= (getField . route_id $ v)
-    , "start_time" .= (getField . start_time $ v)
-    , "start_date" .= (getField . start_date $ v)
+      "trip_id"               .= (getField . trip_id $ v)
+    , "route_id"              .= (getField . route_id $ v)
+    , "start_time"            .= (getField . start_time $ v)
+    , "start_date"            .= (getField . start_date $ v)
     , "schedule_relationship" .= (getField . schedule_relationship $ v)
     ]
 
 instance ToJSON TripUpdate where
   toJSON v = object [
-      "vehicle" .= (getField . vehicle $ v)
-    , "trip" .= (getField . trip $ v)
-    , "stop_time_updates" .= (getField . stop_time_update $ v)
+      "vehicle"           .= (getField . tuVehicle $ v)
+    , "trip"              .= (getField . tuTrip $ v)
+    , "stop_time_updates" .= (getField . tuStopTimeUpdate $ v)
     ]
 
 instance ToJSON VehicleDescriptor where
   toJSON v = object [
-      "id" .= (getField . vehicleId $ v)
+      "id"    .= (getField . vehicleId $ v)
     , "label" .= (getField . vehicleLabel $ v)
     ]
 
 instance ToJSON StopTimeUpdate where
   toJSON v = object [
-      "stop_sequence" .= (getField . stop_sequence $ v) 
-    , "stop_id" .= (getField . stop_id $ v)
-    , "arrival" .= (getField . arrival $ v)
-    , "departure" .= (getField . departure $ v)
+      "stop_sequence"         .= (getField . stop_sequence $ v)
+    , "stop_id"               .= (getField . stop_id $ v)
+    , "arrival"               .= (getField . arrival $ v)
+    , "departure"             .= (getField . departure $ v)
     , "schedule_relationship" .= (getField . stScheduleRelationship $ v)
     ]
 
@@ -42,8 +42,8 @@ instance ToJSON STScheduleRelationship where toJSON = String . T.pack . show
     
 instance ToJSON StopTimeEvent where
   toJSON v = object [
-      "delay" .= (getField . delay $ v)
-    , "time" .= (getField . time $ v)
+      "delay"       .= (getField . delay $ v)
+    , "time"        .= (getField . time $ v)
     , "uncertainty" .= (getField . uncertainty $ v)
     ]
 
--- GTFS/Protobuf.hs
+++ GTFS/Protobuf.hs
@@ -14,45 +14,50 @@ data FeedMessage = FeedMessage {
 instance Decode FeedMessage
 
 data FeedHeader = FeedHeader {
-    gtfsRealTimeVersion :: Required 1 (Value Text)
-  , incrementality :: Optional 2 (Enumeration Incrementality)
-  , feedHeaderTimestamp :: Optional 3 (Value Int64)
+    gtfsRealTimeVersion  :: Required 1 (Value Text)
+  , incrementality       :: Optional 2 (Enumeration Incrementality)
+  , feedHeaderTimestamp  :: Optional 3 (Value Int64)
   } deriving (Generic, Show)
 
 instance Decode FeedHeader
 
-data Incrementality = FullDataset | Differential deriving (Show, Ord, Eq, Enum)
+data Incrementality = FullDataset | Differential 
+  deriving (Show, Ord, Eq, Enum)
 
 data FeedEntity = FeedEntity {
-    feedId :: Required 1 (Value Text)
-  , feedIsDeleted :: Optional 2 (Value Bool)
-  , tripUpdate :: Optional 3 (Message TripUpdate)
+    feedId         :: Required 1 (Value Text)
+  , feedIsDeleted  :: Optional 2 (Value Bool)
+  , tripUpdate     :: Optional 3 (Message TripUpdate)
+  , vehicle        :: Optional 4 (Message VehiclePosition)
+  , alert          :: Optional 5 (Message Alert)
   } deriving (Generic, Show)
 
 instance Decode FeedEntity
 
+------------------------------------------------------------------------
+
 data TripDescriptor = TripDescriptor {
-    trip_id :: Optional 1 (Value Text)
-  , route_id :: Optional 5 (Value Text)
-  , start_time :: Optional 2 (Value Text)
-  , start_date :: Optional 3 (Value Text)
-  , schedule_relationship :: Optional 4 (Enumeration TDScheduleRelationship)
+    trip_id                :: Optional 1 (Value Text)
+  , route_id               :: Optional 5 (Value Text)
+  , start_time             :: Optional 2 (Value Text)
+  , start_date             :: Optional 3 (Value Text)
+  , schedule_relationship  :: Optional 4 (Enumeration TDScheduleRelationship)
   -- extensions
   } deriving (Generic, Show)
 
 instance Decode TripDescriptor
 
 data TripUpdate = TripUpdate {
-    trip :: Required 1 (Message TripDescriptor)
-  , vehicle :: Optional 3 (Message VehicleDescriptor)
-  , stop_time_update :: Repeated 2 (Message StopTimeUpdate)
-  , timestamp :: Optional 4 (Value Int64)
+    tuTrip           :: Required 1 (Message TripDescriptor)
+  , tuVehicle        :: Optional 3 (Message VehicleDescriptor)
+  , tuStopTimeUpdate :: Repeated 2 (Message StopTimeUpdate)
+  , tuTimeStamp      :: Optional 4 (Value Int64)
   } deriving (Generic, Show)
 
 instance Decode TripUpdate 
 
 data VehicleDescriptor = VehicleDescriptor {
-    vehicleId :: Optional 1 (Value Text)
+    vehicleId    :: Optional 1 (Value Text)
   , vehicleLabel :: Optional 2 (Value Text)
   , licensePlate :: Optional 3 (Value Text)
   -- extensions
@@ -61,12 +66,11 @@ data VehicleDescriptor = VehicleDescriptor {
 instance Decode VehicleDescriptor 
 
 data StopTimeUpdate = StopTimeUpdate {
-    stop_sequence :: Optional 1 (Value Int32)
-  , stop_id :: Optional 4 (Value Text)
-  , arrival :: Optional 2 (Message StopTimeEvent)
-  , departure :: Optional 3 (Message StopTimeEvent)
-  -- note default is not implemented yet in library
-  , stScheduleRelationship :: Optional 5 (Enumeration STScheduleRelationship)
+    stop_sequence          :: Optional 1 (Value Int32)
+  , stop_id                :: Optional 4 (Value Text)
+  , arrival                :: Optional 2 (Message StopTimeEvent)
+  , departure              :: Optional 3 (Message StopTimeEvent)
+  , stScheduleRelationship :: Optional 5 (Enumeration STScheduleRelationship)  -- note default is not implemented yet in library
   } deriving (Generic, Show)
 
 instance Decode StopTimeUpdate 
@@ -85,4 +89,107 @@ data StopTimeEvent = StopTimeEvent {
   } deriving (Generic, Show)
 
 instance Decode StopTimeEvent
+
+------------------------------------------------------------------------
+
+data VehiclePosition = VehiclePosition {
+    vpTrip                   :: Optional 1 (Message TripDescriptor)
+  , vpVehicle                :: Optional 3 (Message VehicleDescriptor)
+  , vpPosition               :: Optional 2 (Message Position)
+  , vpCurrentStopSequence    :: Optional 3 (Value Int32)
+  , vpStopId                 :: Optional 7 (Value String)
+  , vpVehicleStopStatus      :: Optional 4 (Enumeration VehicleStopStatus)
+  , vpTimeStamp              :: Optional 5 (Value Int64)
+  , vpCongestionLevel        :: Optional 6 (Enumeration  CongestionLevel)
+  } deriving (Generic, Show)
+
+instance Decode VehiclePosition
+                            
+data Position = Position {
+    latitude                 :: Required 1 (Value Float)
+  , longitude                :: Required 2 (Value Float)
+  , bearing                  :: Optional 3 (Value Float)
+  , odometer                 :: Optional 4 (Value Double)
+  , speed                    :: Optional 5 (Value Float)
+  } deriving (Generic, Show)
+                            
+instance Decode Position
+
+data VehicleStopStatus = IncomingAt | StoppedAt | InTransitTo 
+  deriving (Show, Ord, Eq, Enum)
+
+data CongestionLevel = UnknownCongestionLevel 
+                     | RunningSmoothly
+                     | StopAndGo
+                     | Congestion
+                     | SevereCongestion
+                     deriving (Show, Ord, Eq, Enum)
+
+------------------------------------------------------------------------
+
+data Alert = Alert {
+    alertTimeRange           :: Repeated 1 (Message TimeRange)
+  , alertEntitySelector      :: Repeated 5 (Message EntitySelector)
+  , alertCause               :: Optional 6 (Enumeration Cause)
+  , alertEffect              :: Optional 7 (Enumeration Effect)
+  , alertUrl                 :: Optional 8 (Message TranslatedString)
+  , alertDescriptionText     :: Optional 11 (Message TranslatedString)
+  } deriving (Generic, Show)
+
+instance Decode Alert
+                            
+data TimeRange = TimeRange {
+    timeRangeStart :: Optional 1 (Value Int64)
+  , timeRangeEnd   :: Optional 2 (Value Int64)
+  } deriving (Generic, Show)
+
+instance Decode TimeRange
+
+data EntitySelector = EntitySelector {
+    esAgencyId :: Optional 1 (Value String)
+  , esRouteId :: Optional 2 (Value String)
+  , esRouteType :: Optional 3 (Value Int32)
+  , esTrip :: Optional 4 (Message TripDescriptor)
+  , esStopId :: Optional 5 (Value String)
+  } deriving (Generic, Show)
+
+instance Decode EntitySelector
+
+data Cause = UnknownCause
+           | OtherCause
+           | TechnicalProblem
+           | Strike
+           | Demonstration
+           | Accident
+           | Holiday
+           | Weather
+           | Maintenance
+           | Construction
+           | PoliceActivity
+           | MedicalEmergency
+           deriving (Show, Ord, Eq, Enum)
+
+data Effect = NoService
+            | ReducedService
+            | SignificantDelays
+            | Detour
+            | AdditionalService
+            | ModifiedService
+            | OtherEffect
+            | UnknownEffect
+            | StopMoved
+            deriving (Show, Ord, Eq, Enum)
+
+data TranslatedString = TranslatedString {
+    translation :: Repeated 1 (Message Translation)
+    } deriving (Generic, Show)
+
+instance Decode TranslatedString
+
+data Translation = Translation {
+    translationText     :: Required 1 (Value String)
+  , translationLanguage :: Optional 2 (Value String)
+  } deriving (Generic, Show)
+
+instance Decode Translation
 
